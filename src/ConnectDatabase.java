669928c090164e84705edcce3db0c8c599cb50b7
yue-b <yue@byuedeMBP.lan>
2015-12-10 23:16:51 +0800

    Initial

---
+++ src/ConnectDatabase.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.mysql.jdbc.ResultSet;
+
+public class ConnectDatabase {
+	public String url = "jdbc:mysql://localhost:3307/university";
+	public String name = "com.mysql.jdbc.Driver";
+	public String username = "root";
+	public String password = "8387110";
+	public java.sql.PreparedStatement pst = null;
+	public Connection connection = null;
+	public ResultSet ret = null;
+	public java.sql.Statement statement = null;
+	void connect(){
+		try
+		{
+			Class.forName(name); // 制定连接类型
+			connection = DriverManager.getConnection(url, username, password);// 获取连接
+			System.out.println("mysql connection is ok");
+		} catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
